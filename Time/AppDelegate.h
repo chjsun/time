0b2b3aa439033390dc97b8c871868e2402747889
chjsun <scj126yx@126.com>
2018-10-09 11:43:52 +0800

    Initial Commit

---
+++ Time/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Time
+//
+//  Created by 孙传俊 on 2018/10/9.
+//  Copyright © 2018 孙传俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Time/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Time
+//
+//  Created by 孙传俊 on 2018/10/9.
+//  Copyright © 2018 孙传俊. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
